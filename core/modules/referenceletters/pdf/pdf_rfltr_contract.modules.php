ff8aa559d3c042f30fe441a386550f9aacdef30b
Florian HENRY <florian.henry@open-concept.pro>
2014-08-13 16:25:43 +0200

    0.4

--- core/modules/referenceletters/pdf/pdf_rfltr_contract.modules.php
+++ core/modules/referenceletters/pdf/pdf_rfltr_contract.modules.php
@@ -229,6 +229,15 @@ class pdf_rfltr_contract extends ModelePDFReferenceLetters
 						$chapter_text=str_replace(array_keys($substitution_array), array_values($substitution_array), $chapter_text);
 					}
 					
+					$tmparray=$this->get_substitutionarray_other($outputlangs);
+					$substitution_array=array();
+					if (is_array($tmparray) && count($tmparray)>0) {
+						foreach($tmparray as $key=>$value) {
+							$substitution_array['{'.$key.'}']=$value;
+						}
+						$chapter_text=str_replace(array_keys($substitution_array), array_values($substitution_array), $chapter_text);
+					}
+					
 					$test=$pdf->writeHTMLCell(0, 0, $posX, $posY, $outputlangs->convToOutputCharset($chapter_text), 0, 1, false, true);
 					//var_dump($test);
 					if (is_array($line_chapter['options']) && count($line_chapter['options'])>0) {

c233df6366c0124ce927c2bf769049235a763f82
gauthier <gauthier.verdol@atm-consulting.fr>
2017-11-14 16:56:23 +0100

    NEW : liste paiements - gestion bloc totaux & TVA

--- core/modules/referenceletters/modules_referenceletters.php
+++ core/modules/referenceletters/modules_referenceletters.php
@@ -182,7 +182,7 @@ abstract class ModelePDFReferenceLetters extends CommonDocGeneratorReferenceLett
 						$txt = str_replace(array_keys($substitution_array), array_values($substitution_array), $txt);
 					}
 
-					$tmparray = $this->get_substitutionarray_other($this->outputlangs);
+					$tmparray = $this->get_substitutionarray_other($this->outputlangs, $object);
 					$substitution_array = array();
 					if (is_array($tmparray) && count($tmparray) > 0) {
 						foreach ( $tmparray as $key => $value ) {

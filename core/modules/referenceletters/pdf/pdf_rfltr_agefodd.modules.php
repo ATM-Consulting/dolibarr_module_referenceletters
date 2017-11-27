7d9f4431fc3b887b9a703dff23bb63a3021a2cf5
atm-greg <gregory.blemand@atm-consulting.fr>
2017-11-27 10:10:55 +0100

    correction erreur 500 génération pdf agefodd

--- core/modules/referenceletters/pdf/pdf_rfltr_agefodd.modules.php
+++ core/modules/referenceletters/pdf/pdf_rfltr_agefodd.modules.php
@@ -229,7 +229,8 @@ class pdf_rfltr_agefodd  extends ModelePDFReferenceLetters {
 									        while (@strpos($strong, '<span style=', $i) !== false) {
 									            $len = strpos(substr($strong,strpos($strong, '<span style="', $i) + 13), '">', $i) - strpos($strong, '<span style="', $i);
 									            $style .= substr($strong, strpos($strong, '<span style="', $i) + 13, $len) . ';';
-									            if(empty(strpos($strong, '<span style="', $i))){
+									            $styleposition = strpos($strong, '<span style=', $i);
+									            if(empty($styleposition)){
 									                $l = strripos($strong, '</span>', $i) - strpos($strong, '>', $i) -1;
 									                $strong = substr($strong, strpos($strong, '>', $o) +1, $l); 
 									            } else {

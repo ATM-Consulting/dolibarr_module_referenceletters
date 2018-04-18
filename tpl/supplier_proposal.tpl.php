5f9da400d493d5fb45f2604941d131d9e7453ca7
florian HENRY <florian.henry@atm-consulting.fr>
2018-04-18 16:50:09 +0200

    add files

---
+++ tpl/supplier_proposal.tpl.php
@@ -0,0 +1,19 @@
+<?php
+	print '<table class="border" width="100%">';
+
+	$linkback = '<a href="' . DOL_URL_ROOT . '/supplier_proposal/list.php' . (! empty($socid) ? '?socid=' . $socid : '') . '">' . $langs->trans("BackToList") . '</a>';
+
+	$soc=$object->thirdparty;
+
+	// Ref
+	print '<tr><td>' . $langs->trans('Ref') . '</td><td colspan="5">';
+	print $form->showrefnav($object, 'ref', $linkback, 1, 'ref', 'ref', '');
+	print '</td></tr>';
+
+	print '<tr><td>' . $langs->trans('Company') . '</td><td colspan="5">' . $soc->getNomUrl(1) . '</td>';
+	print '</tr>';
+
+	// Statut
+	print '<tr><td height="10">' . $langs->trans('Status') . '</td><td align="left" colspan="2">' . $object->getLibStatut(4) . '</td></tr>';
+
+	print '</table><br>';

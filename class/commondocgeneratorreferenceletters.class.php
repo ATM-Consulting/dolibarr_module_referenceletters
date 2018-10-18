b4baf497e75d156340205c6a930c699d4ec27a17
gauthier <gauthier.verdol@atm-consulting.fr>
2018-10-18 12:31:34 +0200

    FIX : label devise

--- class/commondocgeneratorreferenceletters.class.php
+++ class/commondocgeneratorreferenceletters.class.php
@@ -141,6 +141,8 @@ class CommonDocGeneratorReferenceLetters extends CommonDocGenerator
 			}
 		}
 
+		if(!empty($object->multicurrency_code)) $resarray['devise_label'] = currency_name($object->multicurrency_code);
+
 		return $resarray;
 	}
 

78f6f42fb1e9f31b7bf13577a0df285fb8581b92
lvessiller <lvessiller@open-dsi.fr>
2020-11-18 15:58:57 +0100

    FIX html encode

--- core/modules/referenceletters/modules_referenceletters.php
+++ core/modules/referenceletters/modules_referenceletters.php
@@ -468,7 +468,7 @@ abstract class ModelePDFReferenceLetters extends CommonDocGeneratorReferenceLett
 								$oldline = $listlines->xml;
 								foreach ($tmparray as $key => $val) {
 									try {
-										$listlines->setVars($key, $val, false, 'UTF-8');
+										$listlines->setVars($key, $val, true, 'UTF-8');
 									} catch (OdfException $e) {
 									} catch (SegmentException $e) {
 									}

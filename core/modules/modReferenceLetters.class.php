b147f52144ab5bf3656146edf825aa495cf6a6cd
John BOTELLA <john.botella@atm-consulting.fr>
2021-06-14 11:10:01 +0200

    Update modReferenceLetters.class.php

--- core/modules/modReferenceLetters.class.php
+++ core/modules/modReferenceLetters.class.php
@@ -51,7 +51,7 @@ class modReferenceLetters extends DolibarrModules
 
 		// Family can be 'crm','financial','hr','projects','products','ecm','technic','other'
 		// It is used to group modules in module setup page
-		$this->family = 'ATM Consulting - Agefodd';
+		$this->family = 'ATM Consulting – Autres';
 		// Module label (no space allowed)
 		// used if translation string 'ModuleXXXName' not found
 		// (where XXX is value of numeric property 'numero' of module)

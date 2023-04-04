43e41f159b57c5d25d9c159f91f8b4fe56b7b97e
ATM-NicolasV <nicolas.vidal@atm-consulting.fr>
2023-04-04 12:18:07 +0200

    FIX : Add substitution in agefodd to be able to use commercial mobile phone

--- core/modules/modReferenceLetters.class.php
+++ core/modules/modReferenceLetters.class.php
@@ -61,7 +61,7 @@ class modReferenceLetters extends DolibarrModules
 		// (where XXX is value of numeric property 'numero' of module)
 		$this->description = "Description of module ReferenceLetters";
 		// Possible values for version are: 'development', 'experimental' or version
-		$this->version = '2.11.0';
+		$this->version = '2.11.1';
 		// Key used in llx_const table to save module status enabled/disabled
 		// (where MYMODULE is value of property name of module in uppercase)
 		$this->const_name = 'MAIN_MODULE_' . strtoupper($this->name);

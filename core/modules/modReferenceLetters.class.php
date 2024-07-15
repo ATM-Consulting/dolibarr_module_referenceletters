e4e7d61320f9708161f27214e8bdd5cbbc024303
Jonathan lescaut <jonathan.lescaut@atm-consulting.fr>
2024-07-15 13:34:40 +0200

    compatV20

--- core/modules/modReferenceLetters.class.php
+++ core/modules/modReferenceLetters.class.php
@@ -63,7 +63,7 @@ class modReferenceLetters extends DolibarrModules
 		// Possible values for version are: 'development', 'experimental' or version
 
 
-		$this->version = '2.21.4';
+		$this->version = '2.22.0';
 		// Url to the file with your last numberversion of this module
 		require_once __DIR__ . '/../../class/techatm.class.php';
 		$this->url_last_version = \referenceletters\TechATM::getLastModuleVersionUrl($this);
@@ -158,7 +158,7 @@ class modReferenceLetters extends DolibarrModules
 		);
 		// Minimum version of Dolibarr required by module
 		$this->need_dolibarr_version = array (
-				12,
+				16,
 				0
 		);
 		$this->langfiles = array (

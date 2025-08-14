c6a03ad04e3af97cf974fb34ccc8ec15712e685a
ATM-nicolasV <nicolas.vidal@atm-consutling.fr>
2025-08-14 09:56:23 +0200

    - FIX : Line substituion, delete instance of

--- core/modules/modReferenceLetters.class.php
+++ core/modules/modReferenceLetters.class.php
@@ -62,7 +62,7 @@ class modReferenceLetters extends DolibarrModules
 		$this->description = 'DocEdit (a.k.a. ReferenceLetters) allows you to create PDF templates using a wysiwyg editor';
 		// Possible values for version are: 'development', 'experimental' or version
 
-		$this->version = '2.23.12';
+		$this->version = '2.23.13';
 		// Url to the file with your last numberversion of this module
 		require_once __DIR__ . '/../../class/techatm.class.php';
 		$this->url_last_version = \referenceletters\TechATM::getLastModuleVersionUrl($this);
